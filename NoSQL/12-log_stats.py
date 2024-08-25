55dde46561f379607c3b53aac39fe14becb2ed0a
Your Name <you@example.com>
2024-08-25 09:57:08 +0000

    added

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,39 +1,23 @@
 #!/usr/bin/env python3
 """
-Provide some stats about Nginx logs stored in MongoDB
-Database: logs, Collection: nginx, Display same as example
-first line: x logs, x number of documents in this collection
-second line: Methods
-5 lines with method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-one line with method=GET, path=/status
+Checking Nginx stats
 """
-from pymongo import MongoClient
-
-
-METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-def log_stats(mongo_collection, option=None):
-    """
-    Prototype: def log_stats(mongo_collection, option=None):
-    Provide some stats about Nginx logs stored in MongoDB
-    """
-    items = {}
-    if option:
-        value = mongo_collection.count_documents(
-            {"method": {"$regex": option}})
-        print(f"\tmethod {option}: {value}")
-        return
-
-    result = mongo_collection.count_documents(items)
-    print(f"{result} logs")
-    print("Methods:")
-    for method in METHODS:
-        log_stats(nginx_collection, method)
-    status_check = mongo_collection.count_documents({"path": "/status"})
-    print(f"{status_check} status check")
+import pymongo
 
 
 if __name__ == "__main__":
-    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
-    log_stats(nginx_collection)
-    
+    """
+    Logs...
+    """
+    client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+    doc_count = collection.estimated_document_count()
+    print("{} logs".format(doc_count))
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        method_count = collection.count_documents({'method': method})
+        print("\tmethod {}: {}".format(method, method_count))
+    status_count = collection.count_documents(
+                {'method': 'GET', 'path': "/status"})
+    print("{} status check".format(status_count)) 
